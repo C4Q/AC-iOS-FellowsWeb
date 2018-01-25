bdf950dbfbb03fc37720f130182665c25f3d2fb0
Alex Paul <axp_alpaul@yahoo.com>
2018-01-25 15:22:22 -0500

    app shows 3 methods of presenting web content

--- Fellows/View Controller/DetailViewController.swift
+++ Fellows/View Controller/DetailViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import SafariServices
 
 class DetailViewController: UIViewController {
     
     let detailView = DetailView()
     
     private var fellow: Fellow!
+    
+    private var sfSafariVC: SFSafariViewController!
     
     init(fellow: Fellow, image: UIImage) {
         super.init(nibName: nil, bundle: nil)
@@ -32,7 +35,40 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .clear
         view.addSubview(detailView)
+        detailView.safariLinkButton.addTarget(self, action: #selector(launchSafari), for: .touchUpInside)
+        detailView.webViewLinkButton.addTarget(self, action: #selector(showWebView), for: .touchUpInside)
+        detailView.githubLinkButton.addTarget(self, action: #selector(showSFSafariVC), for: .touchUpInside)
         configureNavBar()
+    }
+    
+    // Presenting Web Content (Method 1) - via Safari
+    // Advantage: user is taken away from your app, may not return :-(
+    @objc private func launchSafari() {
+        UIApplication.shared.open(URL(string:"https://developer.apple.com/documentation")!, options: [:]) { (done) in
+            print("safari was launched")
+        }
+    }
+    
+    // Presenting Web Content (Method 2) - via WKWebView
+    // Advantages: developer is able to customize UI, user is still in your app
+    // Disadvantages: you don't access to the Apple's keychain, so the user's credentials is not available
+    @objc private func showWebView() {
+        let urlRequest = URLRequest(url: URL(string:"https://c4q.nyc")!)
+        let webVC = WebViewController(urlRequest: urlRequest)
+        navigationController?.pushViewController(webVC, animated: true)
+    }
+    
+    // Presenting Web Content (Method 3) - via SFSafariViewController
+    // Advantage: * all the benefits of Safari, e.g Apple Keychain access
+    //            Security Note: developers don't have access to user's information
+    //            * users stay within your app
+    //            * third party authentication via (Oauth) e.g logging with Facebook
+    // Disadvantage: n/a
+    @objc private func showSFSafariVC() {
+        sfSafariVC = SFSafariViewController(url: fellow.githubURL)
+        sfSafariVC.delegate = self
+        
+        navigationController?.pushViewController(sfSafariVC, animated: true)
     }
     
     private func configureNavBar() {
@@ -49,5 +85,10 @@ class DetailViewController: UIViewController {
     @objc func dismissView() {
         dismiss(animated: true, completion: nil)
     }
+}
 
+extension DetailViewController: SFSafariViewControllerDelegate {
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        navigationController?.popViewController(animated: true)
+    }
 }
---
+++ Fellows/View Controller/WebViewController.swift
@@ -0,0 +1,55 @@
+//
+//  WebViewController.swift
+//  FellowsWeb
+//
+//  Created by Alex Paul on 1/25/18.
+//  Copyright © 2018 Alex Paul. All rights reserved.
+//
+
+import UIKit
+import WebKit
+import SVProgressHUD
+
+class WebViewController: UIViewController {
+    
+    private var urlRequest: URLRequest!
+    
+    // Ideally this should be in its own view class
+    lazy var webView: WKWebView = {
+        let config = WKWebViewConfiguration()
+        let wv = WKWebView(frame: view.bounds, configuration: config)
+        wv.navigationDelegate = self
+        return wv
+    }()
+    
+    init(urlRequest: URLRequest) {
+        super.init(nibName: nil, bundle: nil)
+        self.urlRequest = urlRequest
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(webView)
+        webView.load(urlRequest)
+        SVProgressHUD.show()
+    }
+}
+
+extension WebViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        DispatchQueue.main.async {
+            SVProgressHUD.dismiss()
+        }
+        print("didFail: \(error)")
+    }
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        DispatchQueue.main.async {
+            SVProgressHUD.dismiss()
+        }
+        print("didFinish: \(navigation.description)")
+    }
+}
